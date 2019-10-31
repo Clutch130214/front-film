368e95993e482ea98ce9d217bd450b22191aea49
Maayv <dandouau@outlook.fr>
2019-10-31 16:42:45 +0100

    nav menu on App and added Card in it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import React from 'react';
+import Card from './components/Card.js';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Navbar, Nav, NavDropdown, Form, FormControl } from 'react-bootstrap'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+    <header className="App-header">
+    <div class="row justify-content-md-center navStyle">
+      <nav class="col-md-9 col-xs-9">
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">SuperFilm</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Films</Nav.Link>
+              <Nav.Link href="#link">Series</Nav.Link>
+              <Nav.Link href="#link">Acteurs</Nav.Link>
+            </Nav>
+            <Form inline>
+              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
+        <Navbar bg="light" expand="lg">
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Action</Nav.Link>
+            <Nav.Link href="#link">Science-fiction</Nav.Link>
+            <Nav.Link href="#link">Romance</Nav.Link>
+            <Nav.Link href="#link">Policier</Nav.Link>
+            <Nav.Link href="#link">Comedie</Nav.Link>
+          </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        </nav>
+      </div>
+    </header>
+    <Card/>
+    <Card/>
+    <Card/>
+  </div>
   );
 }
 
--- src/components/card.js
+++ src/components/card.js
@@ -4,18 +4,20 @@ import React, { Component } from 'react'
 class Card extends Component {
     render() {
         return (
-            <div>
-               <div className='card mb-3' style={{maxWidth: '540px'}}>
+            <div class="row justify-content-md-center">
+              <div class="col-md-9 col-xs-9">
+               <div className='card mb-3 h-60'>
                 <div className='row no-gutters'>
-                    <div className='col-md-4'>
-                    <img src='https://pourlamourdesmotsdotcom.files.wordpress.com/2019/07/capture-de28099c3a9cran-2019-07-12-c3a0-18.15.34.png' className='card-img' alt='...'/>
+                    <div className='col-md-2'>
+                    <img src='https://pourlamourdesmotsdotcom.files.wordpress.com/2019/07/capture-de28099c3a9cran-2019-07-12-c3a0-18.15.34.png' className='card-img img-responsive' alt='...'/>
                     </div>
-                    <div className='col-md-8'>
+                    <div className='col-md-9'>
                     <div className='card-body'>
                         <h5 className='card-title'>Card title</h5>
                         <p className='card-text'>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                     </div>
                     </div>
+                </div>
                 </div>
                 </div>
             </div>
