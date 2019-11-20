1b2371b56b35e665d35242378ac2e88aaf24d2a9
Maayv <dandouau@outlook.fr>
2019-11-20 16:49:57 +0100

    Added onChangeUser on Login

--- src/Login/components/Login.js
+++ src/Login/components/Login.js
@@ -1,36 +1,52 @@
-import React from "react";
+import React, { Component } from "react";
 import '../../css/Login.css';
 import { Button } from 'react-bootstrap'
 
-const Login = () => {
-  return (
-  <div class="simple-login-container">
-    <h2 className="display-2">Connexion</h2>
-    <div class="row">
-      <div class="col-md-12 form-group">
-        <input type="text" class="form-control" placeholder="Login"/>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md-12 form-group">
-        <input type="password" class="form-control" placeholder="Mot de passe"/>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md-12 text-center">
-        <div class="btn-group">
-          <Button href="/dashboard" className="btn-secondary">Retour</Button>
-          <Button type="submit" className="btn-primary">Se connecter</Button>
+class Login extends Component {
+  constructor(props) {
+        super(props)
+        this.state = {
+            user: {},
+        }
+        this.onChangeUser = this.onChangeUser.bind(this)
+    }
+
+    onChangeUser(key, value) {
+        const obj = Object.assign({}, this.state.user)
+        obj[key] = value
+        this.setState({ user: obj })
+    }
+  render() {
+    console.log(this.state.user)
+    return (
+    <div class="simple-login-container">
+      <h2 className="display-2">Connexion</h2>
+        <div class="row">
+          <div class="col-md-12 form-group">
+            <input type="text" class="form-control" onChange={ (e) => this.onChangeUser('user', (e.target.value)) } placeholder="Login"/>
+          </div>
         </div>
-      </div>
+        <div class="row">
+          <div class="col-md-12 form-group">
+            <input type="password" class="form-control"  onChange={ (e) => this.onChangeUser('password', (e.target.value)) } placeholder="Mot de passe"/>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <div class="btn-group">
+              <Button href="/dashboard" className="btn-secondary">Retour</Button>
+              <Button type="submit" className="btn-primary">Se connecter</Button>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <Button href="/" variant="btn-sm">Créer un compte ?</Button>
+          </div>
+        </div>
     </div>
-    <div class="row">
-      <div class="col-md-12 text-center">
-        <Button href="/" variant="btn-sm">Créer un compte ?</Button>
-      </div>
-    </div>
-  </div>
-  );
-};
+    );
+  };
+}
 
 export default Login;
