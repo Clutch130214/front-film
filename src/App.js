dc27d9b8cc27e3bf9b452bc860344d6f0c33da9c
Maayv <dandouau@outlook.fr>
2019-11-20 08:13:49 +0100

    Link Href

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ function App() {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
               <Nav.Link href="#home">Films</Nav.Link>
-              <Nav.Link href="#link">Series</Nav.Link>
-              <Nav.Link href="#link">Acteurs</Nav.Link>
+              <Nav.Link href="#series">Series</Nav.Link>
+              <Nav.Link href="#actors">Acteurs</Nav.Link>
             </Nav>
             <Form inline>
               <FormControl type="text" placeholder="Search" className="mr-sm-2" />
@@ -29,11 +29,11 @@ function App() {
         <Navbar bg="light" expand="lg">
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Action</Nav.Link>
-            <Nav.Link href="#link">Science-fiction</Nav.Link>
-            <Nav.Link href="#link">Romance</Nav.Link>
-            <Nav.Link href="#link">Policier</Nav.Link>
-            <Nav.Link href="#link">Comedie</Nav.Link>
+            <Nav.Link href="#action">Action</Nav.Link>
+            <Nav.Link href="#sciencefiction">Science-fiction</Nav.Link>
+            <Nav.Link href="#romantic">Romance</Nav.Link>
+            <Nav.Link href="#crime">Policier</Nav.Link>
+            <Nav.Link href="#comedies">Comedie</Nav.Link>
           </Nav>
           </Navbar.Collapse>
         </Navbar>
