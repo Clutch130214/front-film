a1107f9502018af1d374b4a36c78867740e51188
Mayv <dandouau@outlook.fr>
2020-01-13 12:22:30 +0100

    Added register service

--- src/Login/components/Register.js
+++ src/Login/components/Register.js
@@ -1,18 +1,43 @@
 import React, { Component } from "react";
 import '../../css/Register.css';
 import { Button } from 'react-bootstrap'
+import RegisterService from '../../components/RegisterService';
 
 class Register extends Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.Register = new RegisterService();
+  }
+
+  handleChange(e) {
+    this.setState(
+      {
+        [e.target.name]: e.target.value
+      }
+    )
+  }
+
+  handleFormSubmit(e) {
+    e.preventDefault();
+  
+    this.Register.register(this.state.lastName,
+                    this.state.firstName,
+                    this.state.login,
+                    this.state.password,
+                    this.state.age)
+}
 
   render() {
     return (
-      <form className="simple-login-container" onSubmit={this.handleSubmit}>
+      <form className="simple-login-container" onSubmit={this.handleFormSubmit}>
         <h2 className="display-2">Création de compte</h2>
         <div className="row">
           <div className="col-md-12 form-group">
             <div className="input-group">
-              <input type="text" className="form-control" placeholder="Nom" />
-              <input type="text" className="form-control" placeholder="Prénom" />
+              <input type="text" className="form-control" onChange={this.handleChange} name="lastName" placeholder="Nom" />
+              <input type="text" className="form-control" onChange={this.handleChange} name="firstName" placeholder="Prénom" />
             </div>
           </div>
         </div>
@@ -20,24 +45,24 @@ class Register extends Component {
         </div>
         <div className="row">
           <div className="col-md-12 form-group">
-            <input type="text" className="form-control" placeholder="Login" />
+            <input type="text" className="form-control" onChange={this.handleChange} name="login" placeholder="Login" />
           </div>
         </div>
         <div className="row">
           <div className="col-md-12 form-group">
-            <input type="password" className="form-control" placeholder="Mot de passe" />
+            <input type="password" className="form-control" onChange={this.handleChange} name="password" placeholder="Mot de passe" />
           </div>
         </div>
         <div className="row">
           <div className="col-md-12 form-group">
-            <input type="number" className="form-control" placeholder="Age" />
+            <input type="number" className="form-control" onChange={this.handleChange} name="age" placeholder="Age" />
           </div>
         </div>
         <div className="row">
           <div className="col-md-12 text-center">
             <div className="btn-group">
               <Button href="/" className="btn-secondary">Retour</Button>
-              <Button type="submit" className="btn-primary">Créer un compte</Button>
+              <Button type="submit" value="SUBMIT" className="btn-primary">Créer un compte</Button>
             </div>
           </div>
         </div>
---
+++ src/components/RegisterService.js
@@ -0,0 +1,36 @@
+import * as APIConfig from '../conf/APIConfig'
+
+export default class RegisterService {
+    constructor(domain) {
+        this.domain = domain || APIConfig.API_URI
+        this.fetch = this.fetch.bind(this)
+        this.register = this.register.bind(this)
+    }
+
+    register(lastName, firstName, login, password, age) {
+        return this.fetch(`${this.domain}/users`, {
+            method: 'POST',
+            body: JSON.stringify({
+                lastName,
+                firstName,
+                login,
+                password,
+                age
+            })
+        })
+    }
+
+    fetch(url, options) {
+        const headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+
+        return fetch(url, {
+            headers,
+            ...options
+        })
+            .then(this._checkStatus)
+            .then(response => response.json())
+    }
+}
